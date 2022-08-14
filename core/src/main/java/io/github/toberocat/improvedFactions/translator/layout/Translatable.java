816cfabfe285277bf2e268034ebba4c42ea472a9
Toberocat <tobias.madlberger@gmail.com>
2022-08-14 21:51:24 +0200

    Fully loaded lang files

--- core/src/main/java/io/github/toberocat/improvedFactions/translator/layout/Translatable.java
+++ core/src/main/java/io/github/toberocat/improvedFactions/translator/layout/Translatable.java
@@ -2,7 +2,12 @@ package io.github.toberocat.improvedFactions.translator.layout;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import io.github.toberocat.improvedFactions.translator.layout.messages.Messages;
 import io.github.toberocat.improvedFactions.translator.layout.meta.Meta;
+import io.github.toberocat.improvedFactions.translator.layout.ranks.XmlRank;
+import io.github.toberocat.improvedFactions.translator.layout.ranks.XmlSingleRank;
+
+import java.util.Map;
 
 @JacksonXmlRootElement(localName = "translatable")
 public class Translatable {
@@ -10,6 +15,8 @@ public class Translatable {
     private String version;
 
     private Meta meta;
+    private Messages messages;
+    private Map<String, XmlRank> ranks;
 
     public Translatable() {
     }
@@ -28,5 +35,21 @@ public class Translatable {
 
     public void setVersion(String version) {
         this.version = version;
+    }
+
+    public Messages getMessages() {
+        return messages;
+    }
+
+    public void setMessages(Messages messages) {
+        this.messages = messages;
+    }
+
+    public Map<String, XmlRank> getRanks() {
+        return ranks;
+    }
+
+    public void setRanks(Map<String, XmlRank> ranks) {
+        this.ranks = ranks;
     }
 }
