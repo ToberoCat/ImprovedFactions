4d158dc87018ecabf41a9944e2974e6488cd9bf8
Tobias Madlberger <tobias.madlberger@htlstp.at>
2023-05-21 19:06:33 +0200

    Made changes to the way settings gui works

--- core/src/main/java/io/github/toberocat/improvedFactions/core/gui/GuiApi.java
+++
@@ -1,8 +0,0 @@
-package io.github.toberocat.improvedFactions.core.gui;
-
-import io.github.toberocat.improvedFactions.core.player.FactionPlayer;
-import org.jetbrains.annotations.NotNull;
-
-public interface GuiApi {
-    void openGui(@NotNull FactionPlayer player, @NotNull String guiId);
-}
