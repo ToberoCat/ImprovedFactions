d7eac31caa766beb8f759d05cd6051ad9453b8de
tobias <tobias.madlberger@gmail.com>
2024-05-02 20:09:22 +0200

    Kicking offline players is now possible again

--- improved-factions-base/src/main/kotlin/io/github/toberocat/improvedfactions/utils/options/PlayerNameOption.kt
+++ improved-factions-base/src/main/kotlin/io/github/toberocat/improvedfactions/utils/options/PlayerNameOption.kt
@@ -2,8 +2,9 @@ package io.github.toberocat.improvedfactions.utils.options
 
 import org.bukkit.Bukkit
 
+
 class PlayerNameOption(index: Int) : ArgumentOptions(index) {
-    override fun validate(arg: String): Boolean = Bukkit.getPlayer(arg) != null
+    override fun validate(arg: String): Boolean = Bukkit.getOfflinePlayer(arg)?.hasPlayedBefore() == true
 
     override fun argDoesntMatchKey(): String = "base.exceptions.arg-is-no-player"
 }
