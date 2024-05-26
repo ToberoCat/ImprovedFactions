a92eb95eb0a292c42e56b5ccdfbe19c80f30d706
tobias <tobias.madlberger@gmail.com>
2024-05-26 14:15:37 +0200

    Added basic home tests

--- improved-factions-base/src/test/kotlin/io/github/toberocat/improvedfactions/unit/ImprovedFactionsTest.kt
+++ improved-factions-base/src/test/kotlin/io/github/toberocat/improvedfactions/unit/ImprovedFactionsTest.kt
@@ -2,9 +2,14 @@ package io.github.toberocat.improvedfactions.unit
 
 import be.seeseemelk.mockbukkit.MockBukkit
 import be.seeseemelk.mockbukkit.ServerMock
+import be.seeseemelk.mockbukkit.WorldMock
 import io.github.toberocat.improvedfactions.ImprovedFactionsPlugin
+import io.github.toberocat.improvedfactions.factions.Faction
+import io.github.toberocat.improvedfactions.factions.FactionHandler
+import org.bukkit.Material
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import java.util.*
 
 
 open class ImprovedFactionsTest {
@@ -22,4 +27,8 @@ open class ImprovedFactionsTest {
     fun tearDown() {
         MockBukkit.unmock()
     }
+
+    fun testWorld() = WorldMock(Material.DIRT, 3)
+
+    fun testFaction(owner: UUID = UUID.randomUUID()) = FactionHandler.createFaction(owner, "TestFaction")
 }
--- improved-factions-base/src/test/kotlin/io/github/toberocat/improvedfactions/unit/modules/home/HomeModuleTest.kt
+++ improved-factions-base/src/test/kotlin/io/github/toberocat/improvedfactions/unit/modules/home/HomeModuleTest.kt
@@ -1,12 +1,36 @@
 package io.github.toberocat.improvedfactions.unit.modules.home
 
+import be.seeseemelk.mockbukkit.WorldMock
+import io.github.toberocat.improvedfactions.factions.FactionHandler
+import io.github.toberocat.improvedfactions.modules.home.HomeModule.setHome
 import io.github.toberocat.improvedfactions.unit.ImprovedFactionsTest
+import io.github.toberocat.toberocore.command.exceptions.CommandException
+import org.bukkit.Location
+import org.bukkit.Material
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.assertThrows
 
 class HomeModuleTest : ImprovedFactionsTest() {
     @Test
-    fun `test home creation`() {
-        val player = server.addPlayer()
-        assert(true)
+    fun `test home creation in claim`() {
+        val world = testWorld()
+        val chunk = world.getChunkAt(0, 0)
+
+        val faction = testFaction()
+        transaction {
+            faction.claim(chunk)
+            assertDoesNotThrow { faction.setHome(chunk.getBlock(0, 0, 0).location) }
+        }
+    }
+
+    @Test
+    fun `no homes allowed outside of claimed regions`() {
+        val faction = testFaction()
+        val world = testWorld()
+        transaction {
+            assertThrows<CommandException> { faction.setHome(Location(world, 0.0, 0.0, 0.0)) }
+        }
     }
 }
