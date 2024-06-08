d8c122e78f1ac96bd3e5ccaed02dc4fadbcce0c7
tobias <tobias.madlberger@gmail.com>
2024-06-08 21:32:13 +0200

    Fixed buckets being used in claims

---
+++ improved-factions-base/src/main/kotlin/io/github/toberocat/improvedfactions/listeners/claim/ClaimBucketListener.kt
@@ -0,0 +1,20 @@
+package io.github.toberocat.improvedfactions.listeners.claim
+
+import org.bukkit.event.EventHandler
+import org.bukkit.event.player.PlayerBucketEmptyEvent
+import org.bukkit.event.player.PlayerBucketEntityEvent
+import org.bukkit.event.player.PlayerBucketEvent
+import org.bukkit.event.player.PlayerBucketFillEvent
+
+class ClaimBucketListener(zoneType: String, sendMessage: Boolean = true) : ProtectionListener(zoneType, sendMessage) {
+    override fun namespace() = "bucket"
+
+    @EventHandler
+    private fun onBucket(event: PlayerBucketEmptyEvent) = protectChunk(event, event.block, event.player)
+
+    @EventHandler
+    private fun onBucket(event: PlayerBucketFillEvent) = protectChunk(event, event.block, event.player)
+
+    @EventHandler
+    private fun onBucket(event: PlayerBucketEntityEvent) = protectChunk(event, event.entity, event.player)
+}
