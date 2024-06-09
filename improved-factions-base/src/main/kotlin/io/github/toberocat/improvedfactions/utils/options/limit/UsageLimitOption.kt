b9c05e8772f8ecdf6ea943cc9ff2a5b9f6c044dc
tobias <tobias.madlberger@gmail.com>
2024-06-09 20:54:33 +0200

    Fixed the issue with a missing transaction

--- improved-factions-base/src/main/kotlin/io/github/toberocat/improvedfactions/utils/options/limit/UsageLimitOption.kt
+++ improved-factions-base/src/main/kotlin/io/github/toberocat/improvedfactions/utils/options/limit/UsageLimitOption.kt
@@ -1,5 +1,6 @@
 package io.github.toberocat.improvedfactions.utils.options.limit
 
+import io.github.toberocat.improvedfactions.database.DatabaseManager.loggedTransaction
 import io.github.toberocat.improvedfactions.translation.sendLocalized
 import io.github.toberocat.toberocore.command.exceptions.CommandException
 import io.github.toberocat.toberocore.command.options.Option
@@ -12,18 +13,24 @@ class UsageLimitOption(
     private val registry: String,
     private val byPassPermissions: String = "factions.bypass.usage-limits.$registry"
 ) : Option {
-    override fun execute(sender: CommandSender, args: Array<String>): Array<String> {
-        return when {
+    override fun execute(sender: CommandSender, args: Array<String>) = loggedTransaction {
+        return@loggedTransaction when {
             limit < 0 -> args
             sender.hasPermission(byPassPermissions) -> args
             sender !is Player -> throw CommandException("base.exceptions.sender-must-be-player", emptyMap())
             else -> {
                 val entry = PlayerUsageLimits.getUsageLimit(registry, sender.uniqueId)
                 if (entry.used >= limit) {
-                    throw CommandException("base.exceptions.command-limit-reached", mapOf("limit" to limit.toString()))
+                    throw CommandException(
+                        "base.exceptions.command-limit-reached",
+                        mapOf("limit" to limit.toString())
+                    )
                 } else {
                     entry.used = entry.used.plus(1)
-                    sender.sendLocalized("base.messages.command-usage-limit", mapOf("left" to (limit - entry.used).toString()))
+                    sender.sendLocalized(
+                        "base.messages.command-usage-limit",
+                        mapOf("left" to (limit - entry.used).toString())
+                    )
                     args
                 }
             }
