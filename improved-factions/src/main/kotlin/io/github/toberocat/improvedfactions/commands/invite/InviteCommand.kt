41a97f78aee66b5fb3ef8a06a24bb771cd7be4f5
tobias <tobias.madlberger@gmail.com>
2024-12-01 00:28:26 +0100

    did a few invite commands

--- improved-factions/src/main/kotlin/io/github/toberocat/improvedfactions/commands/invite/InviteCommand.kt
+++ improved-factions/src/main/kotlin/io/github/toberocat/improvedfactions/commands/invite/InviteCommand.kt
@@ -1,68 +1,59 @@
 package io.github.toberocat.improvedfactions.commands.invite
 
-import io.github.toberocat.improvedfactions.ImprovedFactionsPlugin
+import io.github.toberocat.improvedfactions.annotations.command.CommandCategory
+import io.github.toberocat.improvedfactions.annotations.command.CommandResponse
+import io.github.toberocat.improvedfactions.annotations.command.GeneratedCommandMeta
+import io.github.toberocat.improvedfactions.commands.CommandProcessResult
+import io.github.toberocat.improvedfactions.commands.sendCommandResult
 import io.github.toberocat.improvedfactions.database.DatabaseManager.loggedTransaction
+import io.github.toberocat.improvedfactions.factions.Faction
+import io.github.toberocat.improvedfactions.modules.base.BaseModule
 import io.github.toberocat.improvedfactions.permissions.Permissions
 import io.github.toberocat.improvedfactions.ranks.FactionRank
-import io.github.toberocat.improvedfactions.translation.sendLocalized
+import io.github.toberocat.improvedfactions.ranks.listRanks
 import io.github.toberocat.improvedfactions.user.factionUser
-import io.github.toberocat.improvedfactions.utils.arguments.OfflinePlayerArgument
-import io.github.toberocat.improvedfactions.utils.arguments.entity.RankArgument
-import io.github.toberocat.improvedfactions.annotations.command.CommandCategory
-import io.github.toberocat.improvedfactions.annotations.command.CommandMeta
-import io.github.toberocat.improvedfactions.utils.options.*
-import io.github.toberocat.toberocore.command.PlayerSubCommand
-import io.github.toberocat.toberocore.command.arguments.Argument
-import io.github.toberocat.toberocore.command.exceptions.CommandException
-import io.github.toberocat.toberocore.command.options.ArgLengthOption
-import io.github.toberocat.toberocore.command.options.Options
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 
-@CommandMeta(
-    description = "base.command.invite.description",
-    category = CommandCategory.INVITE_CATEGORY
+@GeneratedCommandMeta(
+    label = "invite",
+    category = CommandCategory.INVITE_CATEGORY,
+    module = BaseModule.MODULE_NAME,
+    responses = [
+        CommandResponse("invitedPlayer"),
+        CommandResponse("playerNoFaction"),
+        CommandResponse("playerInvited"),
+        CommandResponse("noPermission"),
+        CommandResponse("rankNotFound")
+    ]
 )
-class InviteCommand(private val plugin: ImprovedFactionsPlugin) : PlayerSubCommand("invite") {
-    override fun options(): Options = Options.getFromConfig(plugin, "invite") { options, _ ->
-        options.cmdOpt(InFactionOption(true))
-            .cmdOpt(PlayerNameOption(0))
-            .cmdOpt(RankNameOption(1))
-            .cmdOpt(CanManageRankOption(1))
-            .cmdOpt(ArgLengthOption(2))
-            .cmdOpt(FactionPermissionOption(Permissions.SEND_INVITES))
-    }
+abstract class InviteCommand : InviteCommandContext() {
 
-    override fun arguments(): Array<Argument<*>> = arrayOf(
-        OfflinePlayerArgument(),
-        RankArgument()
-    )
+    fun process(
+        inviter: Player,
+        invited: OfflinePlayer,
+        rank: FactionRank?,
+    ): CommandProcessResult {
+        val faction = inviter.factionUser().faction() ?: return playerNoFaction()
 
-    override fun handle(inviter: Player, args: Array<out String>): Boolean {
-        val arguments = parseArgs(inviter, args)
-        val invited = arguments.get<OfflinePlayer>(0) ?: return false
-        val rank = arguments.get<FactionRank>(1) ?: return false
-
-        val faction = loggedTransaction {
-            val faction = inviter.factionUser().faction() ?: throw CommandException(
-                "base.command.invite.player-no-faction", emptyMap()
-            )
-
-            faction.invite(inviter.uniqueId, invited.uniqueId, rank.id.value)
-            return@loggedTransaction faction
+        if (!inviter.factionUser().hasPermission(Permissions.SEND_INVITES)) {
+            return noPermission()
         }
-        inviter.sendLocalized(
-            "base.command.invite.invited-player", mapOf(
-                "player" to (invited.name ?: "unknown")
-            )
+
+        val factionRank = rank ?: faction.getDefaultRank()
+        faction.invite(inviter.uniqueId, invited.uniqueId, factionRank.id.value)
+        inviter.sendCommandResult(
+            invitedPlayer("player" to (invited.name ?: "unknown"))
         )
-        invited.sendLocalized(
-            "base.command.invite.you-have-been-invited", mapOf(
+
+        invited.player?.sendCommandResult(
+            playerInvited(
                 "faction" to faction.name,
                 "inviter" to inviter.displayName,
-                "rank" to rank.name
+                "rank" to factionRank.name
             )
         )
-        return true
+
+        return invitedPlayer("player" to (invited.name ?: "unknown"))
     }
 }
