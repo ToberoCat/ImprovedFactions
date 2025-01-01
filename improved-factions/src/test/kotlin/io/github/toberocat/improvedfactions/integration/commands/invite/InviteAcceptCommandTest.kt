c23bab1e998611df6b233a63ff23abe5ce76d9ad
Tobias Madlberger <tobias.madlberger@gmail.com>
2025-01-01 15:26:21 +0100

    Added Tests to check if the issue
    Signed-off-by: Tobias Madlberger <tobias.madlberger@gmail.com>

--- improved-factions/src/test/kotlin/io/github/toberocat/improvedfactions/integration/commands/invite/InviteAcceptCommandTest.kt
+++ improved-factions/src/test/kotlin/io/github/toberocat/improvedfactions/integration/commands/invite/InviteAcceptCommandTest.kt
@@ -3,37 +3,25 @@ package io.github.toberocat.improvedfactions.integration.commands.invite
 import be.seeseemelk.mockbukkit.entity.PlayerMock
 import io.github.toberocat.improvedfactions.factions.Faction
 import io.github.toberocat.improvedfactions.unit.ImprovedFactionsTest
+import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
-import org.junit.jupiter.params.provider.ValueSource
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
-class InviteCommandProcessorTest : ImprovedFactionsTest() {
+class InviteAcceptCommandTest : ImprovedFactionsTest() {
 
     private lateinit var player1: PlayerMock
-    private lateinit var player2: PlayerMock
     private lateinit var faction: Faction
 
     @BeforeEach
     override fun setUp() {
         super.setUp()
         player1 = createTestPlayer()
-        player2 = createTestPlayer()
         player1.isOp = true
-        player2.isOp = true
 
         faction = testFaction(player1.uniqueId)
-    }
-
-    @ParameterizedTest
-    @ValueSource(booleans = [true, false])
-    fun `test if can invite player`(onlineMode: Boolean) {
-        server.onlineMode = onlineMode
-
-        assertTrue(server.dispatchCommand(player1, "f invite ${player2.name} Member"))
-        assertNotNull(player1.nextMessage())
     }
 
     @ParameterizedTest
@@ -45,11 +33,12 @@ class InviteCommandProcessorTest : ImprovedFactionsTest() {
         "true, TestPlayer_",
         "false, TestPlayer_"
     )
-    fun `test if can invite player with _`(onlineMode: Boolean, playerName: String) {
+    fun `test if can accept invite with _ in name`(onlineMode: Boolean, playerName: String) {
         server.onlineMode = onlineMode
 
         val testPlayer = createTestPlayer(playerName)
-        assertTrue(server.dispatchCommand(player1, "f invite ${testPlayer.name} Member"))
-        assertNotNull(player1.nextMessage())
+        transaction { faction.invite(player1.uniqueId, testPlayer.uniqueId, faction.getDefaultRank().id.value) }
+        assertTrue(server.dispatchCommand(testPlayer, "f inviteaccept ${faction.name}"))
+        assertNotNull(testPlayer.nextMessage())
     }
 }
