fa051b33f2bf7f5e121e5c086bdf0c1c7bfa8670
Tobias Madlberger <tobias.madlberger@gmail.com>
2025-01-01 18:03:36 +0100

    Enforced language keys for compilation
    Signed-off-by: Tobias Madlberger <tobias.madlberger@gmail.com>

--- improved-factions/code-generation/src/main/kotlin/io/github/toberocat/improvedfactions/localization/processor/LocalizationProcessorProvider.kt
+++ improved-factions/code-generation/src/main/kotlin/io/github/toberocat/improvedfactions/localization/processor/LocalizationProcessorProvider.kt
@@ -3,9 +3,11 @@ package io.github.toberocat.improvedfactions.localization.processor
 import com.google.devtools.ksp.processing.SymbolProcessor
 import com.google.devtools.ksp.processing.SymbolProcessorEnvironment
 import com.google.devtools.ksp.processing.SymbolProcessorProvider
+import java.io.File
 
 class LocalizationProcessorProvider : SymbolProcessorProvider {
     override fun create(environment: SymbolProcessorEnvironment): SymbolProcessor {
-        return LocalizationProcessor(environment.codeGenerator, environment.logger, environment.options)
+        val languageFolder = File(environment.options["languageFolder"]!!)
+        return LocalizationProcessor( environment.logger, languageFolder)
     }
 }
